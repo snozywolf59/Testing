ec4107e4136527f7731817c768c49ef9c1d47512
To Huu Bang <120768537+snozywolf59@users.noreply.github.com>
2024-04-03 23:47:15 +0700

    Update Fee.py

--- Fee.py
+++ Fee.py
@@ -1,15 +1,33 @@
-def fee(price: int, lv: int) -> int:
-    if price < 100 or price > 400:
-        return 0
-    if lv > 3 or lv < 1:
-        return 0
-    while lv > 0:
-        price = 2 * price
-        lv = lv - 1
-    return price
+import unittest
+
+
+def fee(price: int, lv: int) -> int:    #1
+    if price < 100 or price > 400:      #2
+        return 0                        #3
+    if lv > 3 or lv < 0:                #4
+        return 0                        #5
+    while lv > 0:                       #6
+        price = 2 * price               #7
+        lv = lv - 1                     #8
+    return price/2                      #9
+
+
+class TestFee(unittest.TestCase):
+    def test_T00(self):
+        self.assertEqual(fee(10, 0), 0)
+
+    def test_T01(self):
+        self.assertEqual(fee(150, 5), 0)
+
+    def test_T02(self):
+        self.assertEqual(fee(200, 0), 100)
+
+    def test_T03(self):
+        self.assertEqual(fee(320, 1), 320)
+
+    def test_T04(self):
+        self.assertEqual(fee(250, 2), 500)
 
 
 if __name__ == '__main__':
-    p = 120
-    lv = 3
-    print(fee(p, lv))
+    unittest.main()
